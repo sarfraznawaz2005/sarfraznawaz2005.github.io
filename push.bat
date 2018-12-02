7afc82dcf7f75c217afafd21d94987fb0ffca79e
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2018-12-02 20:24:06 +0500

    made changes

--- push.bat
+++ push.bat
@@ -1,4 +1,5 @@
-cd _counter
+rem cd _counter
+git pull
 git add .
 git commit -am "made changes"
 git push
--- push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-###########################
-cd _counter
-git add .
-git commit -am "made changes"
-git push
-###########################
-echo Press Enter...
-read
